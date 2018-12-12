0447325b838dfaa4c7fc525a8c221a3c2f97b7e4
vinson.yuan <vinson.yuan@ingcreations.com>
2018-12-12 17:46:43 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,20 +6,7 @@ module.exports = {
     extends: ['plugin:vue/essential', '@vue/prettier'],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'prettier/prettier': [
-            'error',
-            {
-                eslintIntegration: true,
-                stylelintIntegration: true,
-                tabWidth: 4,
-                singleQuote: true,
-                semi: false,
-                bracketSpacing: true
-            }
-        ],
-        // 禁止使用空解构模式
-        'no-empty-pattern': 0
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
     },
     parserOptions: {
         parser: 'babel-eslint'
