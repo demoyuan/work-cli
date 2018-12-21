1a81397814c04b64847d1ad917213b31650e33a1
Vinson Yuan <604540547@qq.com>
2018-12-21 12:03:10 +0800

    change style

--- src/views/index/App.vue
+++ src/views/index/App.vue
@@ -7,11 +7,4 @@
 
 <style lang="scss">
 @import '@/assets/css/base.scss';
-
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-}
 </style>
